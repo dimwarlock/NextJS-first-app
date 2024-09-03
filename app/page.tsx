53d467fff62bdfab3f54911fc13824373d7548a8
dimwarlock <screanvir@gmail.com>
2024-09-02 21:36:56 -0400

    Finalizacion de Optimizaciones

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
+'use client';
 import Image from 'next/image'
 import umbrella from '@/public/images/Logo.png'
 import { Metadata } from 'next'
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
 
-export default async function Home() {
+const HeavyComponent = dynamic(
+  ()=> import('./components/HeavyComponent'),
+  {
+    ssr: false,
+    loading: ()=> <p>Cargando. . .</p>
+  }
+)
+
+export default function Home() {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <main className='relative h-screen'>
       <h1 className='flex justify-center'>Primera App en NextJS</h1>
+
+      <button className='btn btn-primary mb-5' onClick={()=> setVisible(true)}>Importar dinámicamente HeavyComponent</button>
+
+      <button className='btn btn-primary ml-5' onClick={async ()=> {
+        const _ = (await import('lodash')).default;
+
+        const elements = [
+          {name: 'z'},
+          {name: 'y'},
+          {name: 'x'},
+        ];
+
+        const sorted = _.orderBy(elements, ['name']);
+        console.log(sorted)
+        }}
+      >
+        Ordenar array [z,y,x], resultado en Consola
+      </button>
+
+      {isVisible && <HeavyComponent />}
+
       <Image 
         src={umbrella} 
         alt='umbrella'
